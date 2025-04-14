3821188a6f05946e7983520daea6c33ef25d6560
Ganesh231202 <kurdekarganesh386@gmail.com>
2025-04-14 23:50:05 +0530

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,10 @@
+/ Main.java
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        StudentService service = new StudentService();
+        Scanner sc = new Scanner(System.in);
+        // menu options here (add, display, delete)
+    }
+}
